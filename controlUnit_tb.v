26b060fef06a47f19ec13a5fd0f7fffd739d2bd7
unknown <tharindu.samare123@gmail.com>
2021-07-04 19:01:15 +0530

    alu_tb tested , controlunit_tb tested, controlUnit ALU_idle localparam changed from 3'dx to 3'd6

--- controlUnit_tb.v
+++ controlUnit_tb.v
@@ -14,7 +14,7 @@ end
 
 localparam INS_WIDTH = 8;
 
-reg rstN,start,Zout;
+reg rstN,startN,Zout;
 reg [INS_WIDTH-1:0] ins;
 wire [2:0] aluOp;
 wire [3:0] incReg;    // {PC, RC, RP, RQ}
@@ -76,14 +76,10 @@ localparam [31:0]// time duration for each instruction to exicute
         MV_AC_RL_time_duration  =   4;
 
 
-controlUnit #(.INS_WIDTH(INS_WIDTH)) dut(.clk(clk), .rstN(rstN), .start(start),
+controlUnit #(.INS_WIDTH(INS_WIDTH)) dut(.clk(clk), .rstN(rstN), .startN(startN),
             .Zout(Zout), .ins(ins), .aluOp(aluOp), .incReg(incReg), .wrEnReg(wrEnReg),
             .busSel(busSel), .DataMemWrEn(DataMemWrEn), .ZWrEn(ZWrEn), 
             .done(done), .ready(ready));
-
-////////// for easy readability (start) //////////////
-
-
 
 task automatic test_instruction(
     input [31:0] duration,
@@ -96,7 +92,6 @@ task automatic test_instruction(
     Zout <= Z_value;
 
     #(duration * CLK_PERIOD);
-    // repeat(duration)@(posedge clk);
 end
 
 endtask
@@ -107,9 +102,9 @@ initial begin
     rstN <= 1'b0;
     @(posedge clk);
     rstN <= 1'b1;
-    start <= 1'b0;
+    startN <= 1'b0;
     @(posedge clk);
-    start <= 1'b1;
+    startN <= 1'b1;
 
     ////// test NOP
     test_instruction(NOP_time_duration, 1'bX, NOP);
@@ -197,89 +192,6 @@ initial begin         // simulation stop condition
         end
     end
 
-    // repeat(100) @(posedge clk);
-    // $stop;
 end
 
 endmodule : controlUnit_tb
-
-// module controlUnit_tb();
-
-// reg clk,rd,wr,ce;
-// reg [7:0]  addr,data_wr,data_rd;
-// reg [7:0]  read_data;
-
-// initial begin
-//   clk = 0;
-//   read_data = 0;
-//   rd = 0;
-//   wr = 0;
-//   ce = 0;
-//   addr = 0;
-//   data_wr = 0;
-//   data_rd = 0;
-//   // Call the write and read tasks here
-//   #1 cpu_write(8'h11,8'hAA);
-//   #1 cpu_read(8'h11,read_data);
-//   #1 cpu_write(8'h12,8'hAB);
-//   #1 cpu_read(8'h12,read_data);
-//   #1 cpu_write(8'h13,8'h0A);
-//   #1 cpu_read(8'h13,read_data);
-//   #100 $stop;
-// end
-// // Clock Generator
-// always
-//   #1 clk = ~clk;
-// // CPU Read Task
-// task cpu_read (input [7:0]  address, output [7:0] data);
-//   begin
-//     // $display ("%g CPU Read  task with address : %h", $time, address);
-//     // $display ("%g  -> Driving CE, RD and ADDRESS on to bus", $time);
-//     @ (posedge clk);
-//     addr <= address;
-//     ce <= 1;
-//     rd <= 1;
-//     @ (negedge clk);
-//     data <= data_rd;
-//     @ (posedge clk);
-//     addr <= 0;
-//     ce <= 0;
-//     rd <= 0;
-//     $display ("%g CPU Read  data              : %h", $time, data);
-//     $display ("======================");
-//   end
-// endtask
-// // CU Write Task
-// task cpu_write(input [7:0]  address, input [7:0] data);
-//   begin
-//     $display ("%g CPU Write task with address : %h Data : %h", 
-//       $time, address,data);
-//     $display ("%g  -> Driving CE, WR, WR data and ADDRESS on to bus", 
-//       $time);
-//     @ (posedge clk);
-//     addr = address;
-//     ce = 1;
-//     wr = 1;
-//     data_wr = data;
-//     @ (posedge clk);
-//     addr = 0;
-//     ce = 0;
-//     wr = 0;
-//     $display ("======================");
-//   end
-// endtask
-
-// // Memory model for checking tasks
-// reg [7:0] mem [0:255];
-
-// always @ (addr or ce or rd or wr or data_wr)
-// if (ce) begin
-//   if (wr) begin
-//     mem[addr] = data_wr;
-//   end
-//   if (rd) begin
-//     data_rd = mem[addr];
-//   end
-// end
-
-// endmodule
